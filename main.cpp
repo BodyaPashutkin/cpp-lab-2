d3816526319e8854adb8b665232d963204bbbf79
BodyaPashutkin <bspashutkin@edu.hse.ru>
2023-10-10 04:51:57 +0300

    remake

--- main.cpp
+++ main.cpp
@@ -1,50 +1,50 @@
 #include <iostream>
+#include <iomanip>
 
 //делаем степень
-long double pow(long double base, int pow){
-    long double result = 1;
-    for (int i = 0; i < pow; i++){
-        result *= base;
+long double stepen(long double baza, int stepen){
+    long double resultat = 1;
+    int i = 0;
+    while (i<stepen){//for (int i = 0; i < stepen; i++){
+        i++;
+        resultat *= baza;
     }
-    return result;
+    return resultat;
 }
 //делаем двойной факториал
-long double double_factorial(long double value){
-    long double result = 1;
-    for (int k = value; k > 0; k-=2) {
-        result *= k;
+long double double_factorial(long double znachenie){
+    long double resultat = 1;
+    //int k = znachenie;
+    while (znachenie>0){//for (int k = value; k > 0; k-=2) {
+        resultat *= znachenie;
+        znachenie-=2;
     }
-    return result;
+    return resultat;
 }
 //получение t
-long double getT(long double x){
-    long double numerator = 0;
-    long double denominator = 0;
-    for (int k = 0; k<=10; k++){
-        long double subnumerator = pow(x, (2*k+1));
-        long double denominator = double_factorial((2*k+1));
-        numerator += (subnumerator/denominator);
+long double T(long double x){
+    long double chislitel = 0;
+    long double znamenatel = 0;
+    int k = 0;
+    while (k<=10){//for (int k = 0; k<=10; k++){
+        long double podchislitel = stepen(x, (2*k+1));
+        long double podznamenatel = double_factorial((2*k+1));
+        chislitel += (podchislitel/podznamenatel);
+        long double podchislitel_2 = stepen(x, 2*k);
+        long double podznamenatel_2 = double_factorial(2*k);
+        znamenatel += (podchislitel_2/podznamenatel_2);
+        k++;
     }
-    for (int k = 0; k<=10; k++){
-        long double subnumerator = pow(x, 2*k);
-        long double subdenominator = double_factorial(2*k);
-        denominator += (subnumerator/subdenominator);
-    }
-    long double result = (numerator/denominator);
-    return result;
+    return chislitel/znamenatel;
 }
-//Связующая функция
-long double getResult(long double y){
-    long double numerator = (7 * getT(0.25) + 2 * getT(1 + y));
-    long double denominator = (6 - getT(pow(y,2)-1));
-    return numerator/denominator;
-}
+
 //главная функция
 int main(){
     long double y;
-    printf("Введите y: ");
-    scanf("%Lf", &y);
-    long double result = getResult(y);
-    printf("Результат: %.10Lf\n", result);
+    std::cout << "Введите y: " << std::endl;//printf("Введите y: ");
+    std:: cin >> y;//scanf("%Lf", &y);
+    long double chislitel = (7 * T(0.25) + 2 * T(1 + y));
+    long double znamenatel = (6 - T(stepen(y,2)-1));
+    std:: cout << std:: setprecision(11) << chislitel/znamenatel << std::fixed; //printf("Результат: %.10Lf\n", result);
     return 0;
 }
